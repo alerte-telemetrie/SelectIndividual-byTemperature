fafd084314e7548c6af00a7f20d09c25d4bb14fb
alerte-telemetrie <alerte.telemetrie@lpo.fr>
2024-06-20 12:04:21 +0200

    Update RFunction.R

--- RFunction.R
+++ RFunction.R
@@ -1,37 +1,88 @@
 library('move2')
 library('lubridate')
+library('logger')
 
-## The parameter "data" is reserved for the data object passed on from the previous app
+# dur = duration in hours
+# low_temp = threshold below which a temperature is considered extremely low (in °C)
 
-# to display messages to the user in the log file of the App in MoveApps
-# one can use the function from the logger.R file:
-# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+data <- readRDS("C:/Users/typhaine.rousteau/OneDrive - LPO/MoveApps/SelectIndividual-byTemperature/SelectIndividual-byTemperature/tests/testthat/data/input3_move2loc_LatLon.rds")
 
-# Showcase injecting app setting (parameter `year`)
-rFunction = function(data, sdk, year, ...) {
-  logger.info(paste("Welcome to the", sdk))
-  result <- if (any(lubridate::year(mt_time(data)) == year)) { 
-    data[lubridate::year(mt_time(data)) == year,]
-  } else {
-    NULL
-  }
-  if (!is.null(result)) {
-    # Showcase creating an app artifact. 
-    # This artifact can be downloaded by the workflow user on Moveapps.
-    artifact <- appArtifactPath("plot.png")
-    logger.info(paste("plotting to artifact:", artifact))
-    png(artifact)
-    plot(result[mt_track_id_column(result)], max.plot=1)
-    dev.off()
-  } else {
-    logger.warn("nothing to plot")
-  }
-  # Showcase to access a file ('auxiliary files') that is 
-  # a) provided by the app-developer and 
-  # b) can be overridden by the workflow user.
-  fileName <- getAuxiliaryFilePath("auxiliary-file-a")
-  logger.info(readChar(fileName, file.info(fileName)$size))
+rFunction = function(data, time_now = NULL, dur = 24, low_temp = 5)
+  {
+    Sys.setenv(tz = "UTC") 
+  
+    if (is.null(time_now)) time_now <- Sys.time() else time_now <- as.POSIXct(time_now, format = "%Y-%m-%d %H:%M:%OS", tz = "UTC")
+   
+     log_info(paste("You have selected the data of the last", dur,"hours starting from", time_now, "(UTC)."))
+    
+     startTime <- time_now - lubridate::hours(dur)
 
-  # provide my result to the next app in the MoveApps workflow
-  return(result)
+     # Splits the stack to account for various individuals
+     splitStack <- split(data, mt_track_id(data))
+    
+     # Helper function to analyse the different stacks separately
+     helperFunction = function(splitMoveStack) {
+      
+      # Initializes the variables
+      Ind <- c()
+      Min_temp <- c()
+      
+      # data of the defined last hours
+      data_dur <- splitMoveStack[which(splitMoveStack$timestamp > startTime ), ]
+      
+      # For ornitela tags ------------------------------------------------------
+      ornitela_data <- data_dur[which(!is.na(data_dur$external_temperature)), ]
+      
+      if(nrow(data_dur) > 0 & nrow(ornitela_data) > 0) { 
+        
+        # Gets min temperature
+        data_dur <- data_dur[which(data_dur$ornitela_transmission_protocol == "GPRS"),]
+        min_temp <- as.numeric(min(data_dur$external_temperature))
+        
+        # Is the temperature over the last defined hours below the minimum temperature threshold ?
+        if( min_temp <- low_temp) {
+            Min_temp <- append(Min_temp, min_temp)
+            Ind <- append(Ind, unique(mt_track_id(splitMoveStack)))
+        }
+        
+      }
+      
+      # For other tags ---------------------------------------------------------
+      other_data <- data_dur[which(!is.na(data_dur$eobs_temperature)), ]
+      
+      if(nrow(data_dur) > 0 & nrow(other_data) > 0) { 
+        
+        # Gets min temperature
+        min_temp <- as.numeric(min(data_dur$eobs_temperature))
+        
+        # Is the temperature over the last defined hours below the minimum temperature threshold ?
+        if(min_temp <- low_temp) {
+           Min_temp <- append(Min_temp, min_temp)
+           Ind <- append(Ind, unique(mt_track_id(splitMoveStack)))
+        }
+        
+      }
+      
+      return(data.frame(Ind, Min_temp, row.names=NULL))
+     
+      }
+     
+        output <- lapply(splitStack, helperFunction)	
+
+        # Gathers the results in a data frame
+        output <- do.call("rbind", output)
+        
+        if(nrow(output > 0)) {
+          
+          log_warn(paste("You have", nrow(output),"individuals whose tag recorded a temperature below", low_temp, "°C."))
+          
+          # Writes the csv
+          write.csv(output, appArtifactPath("Low_Temperature_Animals.csv"), row.names = FALSE)
+          
+        } else {
+          
+          log_info("You have no individuals whose tag recorded a temperature below ", low_temp, "°C.")
+          
+        }
+     
 }
